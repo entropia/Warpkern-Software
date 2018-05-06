5b1b9988e46a5971e628c3d71b6f2dac6994af7d
DrLuke <Lukas@Jackowski.de>
2018-05-06 17:28:11 +0200

    Fix data writing

--- warpkern_rpi.py
+++ warpkern_rpi.py
@@ -6,7 +6,7 @@ from typing import List
 import random
 
 def floatToByte(val: float) -> int:
-    return max(0, min(int(val*255 + random.random() - 0.5), 255)) # Convert float to byte + temporal dithering
+    return max(0, min(int(val*255 + (random.random() - 0.5)*0.95), 255)) # Convert float to byte + temporal dithering
 
 class PiPhy(WarpkernPhy):
     def __init__(self):
@@ -17,9 +17,13 @@ class PiPhy(WarpkernPhy):
     def pushData(self, data: List[List[float]]):
         bytedata = [0, 0, 0, 0]     # Startframe
         for pix in data:
-            bytedata += [floatToByte(1),
+            bytedata += [255,
                         floatToByte(pix[0]),
                         floatToByte(pix[1]),
                         floatToByte(pix[2])]
 
-        wiringpi.wiringPiSPIDataRW(0, bytes(bytedata))
+        while(len(bytedata) > 1000):    # Write in chuncks of 1000
+            dataout = bytedata[0:1000]
+            wiringpi.wiringPiSPIDataRW(0, bytes(dataout))
+            bytedata = bytedata[1000:]
+        wiringpi.wiringPiSPIDataRW(0, bytes(bytedata))  # write the last chunk

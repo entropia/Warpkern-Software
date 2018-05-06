b8e726911f6e549bd4fc68e9fb7ec70d919c3334
DrLuke <Lukas@Jackowski.de>
2018-05-06 23:20:14 +0200

    Add timing measurement

--- warpkern_rpi.py
+++ warpkern_rpi.py
@@ -6,6 +6,8 @@ from typing import List
 import random
 import math
 
+from time import time
+
 def floatToByte(val: float) -> int:
     return max(0, min(int(val*255), 255)) # Convert float to byte + temporal dithering
 
@@ -16,19 +18,31 @@ class PiPhy(WarpkernPhy):
         wiringpi.wiringPiSPISetup(0, 4800000)
 
     def pushData(self, data: List[float]):
+        t = time()
         bytedata = [0, 0, 0, 0]     # Startframe
 
         for i in range(len(data)//3):
             bytedata += [255,
-                        floatToByte(data[i*3+2]),
-                        floatToByte(data[i*3+1]),
-                        floatToByte(data[i*3])]
+                         floatToByte(data[i*3+2]),
+                         floatToByte(data[i*3+1]),
+                         floatToByte(data[i*3])]
+
+        print("Convert data: %s" % (time() - t))
+        t = time()
 
         for i in range(int(math.ceil(len(bytedata)/8))):
             bytedata.append(1)  # Add padding at end
+
+        print("Padding: %s" % (time() - t))
+        t = time()
 
         while(len(bytedata) > 4000):    # Write in chuncks of 1000
             dataout = bytedata[0:4000]
             wiringpi.wiringPiSPIDataRW(0, bytes(dataout))
             bytedata = bytedata[4000:]
         wiringpi.wiringPiSPIDataRW(0, bytes(bytedata))  # write the last chunk
+
+        print("Write: %s" % (time() - t))
+        t = time()
+
+        print("push over")

f829bb45ffc4249c581c717610b796cbd527e653
DrLuke <Lukas@Jackowski.de>
2018-05-06 22:57:26 +0200

    Fix byte unpacking

--- warpkern_rpi.py
+++ warpkern_rpi.py
@@ -15,19 +15,20 @@ class PiPhy(WarpkernPhy):
 
         wiringpi.wiringPiSPISetup(0, 4800000)
 
-    def pushData(self, data: List[List[float]]):
+    def pushData(self, data: List[float]):
         bytedata = [0, 0, 0, 0]     # Startframe
-        for pix in data:
+
+        for i in range(len(data)//3):
             bytedata += [255,
-                        floatToByte(pix[2]),
-                        floatToByte(pix[1]),
-                        floatToByte(pix[0])]
+                        floatToByte(data[i+2]),
+                        floatToByte(data[i+1]),
+                        floatToByte(data[i])]
 
         for i in range(int(math.ceil(len(bytedata)/8))):
             bytedata.append(1)  # Add padding at end
 
-        while(len(bytedata) > 1000):    # Write in chuncks of 1000
-            dataout = bytedata[0:1000]
+        while(len(bytedata) > 4000):    # Write in chuncks of 1000
+            dataout = bytedata[0:4000]
             wiringpi.wiringPiSPIDataRW(0, bytes(dataout))
-            bytedata = bytedata[1000:]
+            bytedata = bytedata[4000:]
         wiringpi.wiringPiSPIDataRW(0, bytes(bytedata))  # write the last chunk

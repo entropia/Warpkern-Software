f61c22f62eb0af93a16614ee236a4cd71769ab8d
DrLuke <Lukas@Jackowski.de>
2018-05-07 12:39:16 +0200

    Try threading

--- warpkern_rpi.py
+++ warpkern_rpi.py
@@ -8,7 +8,12 @@ import math
 
 from time import time
 
+from threading import Thread
+
 import numpy as np
+
+def writeData(data):
+    wiringpi.wiringPiSPIDataRW(0, data)  # write the last chunk
 
 def floatToByte(val: float) -> int:
     return max(0, min(int(val*255), 255)) # Convert float to byte + temporal dithering
@@ -20,4 +25,8 @@ class PiPhy(WarpkernPhy):
         wiringpi.wiringPiSPISetup(0, 4800000)
 
     def pushData(self, data: np.array):
-        wiringpi.wiringPiSPIDataRW(0, data.tobytes())  # write the last chunk
+        t = Thread(target = writeData, args=(data.tobytes()))
+        t.start()
+
+
+
